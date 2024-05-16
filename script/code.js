acb49a67205ea93599800e54a10faef4f42df8a5
LeahBasson <leahbasson54@gmail.com>
2024-05-16 23:56:22 +0200

    Calculator

--- script/code.js
+++ script/code.js
@@ -7,9 +7,18 @@ result.addEventListener('click', () =>
 
     let bmi = (weight / ((height*height)/10000)).toFixed(2);
 
-    console.log(bmi);
-
-    document.querySelector(`[output]`).innerHTML= bmi;
+    if(bmi<= 18.4){
+        document.querySelector(`[output]`).innerHTML = 'Underweight : ' + bmi;
+    }
+    else if (bmi >= 18.5 && bmi < 24.9 ){
+        document.querySelector(`[output]`).innerHTML = 'Normal : ' + bmi;  
+    }
+    else if (bmi >= 25.0 && bmi < 39.9){
+        document.querySelector(`[output]`).innerHTML = 'Overweight : ' + bmi;  
+    }
+    else if (bmi >= 40.0){
+        document.querySelector(`[output]`).innerHTML = 'Obese : ' + bmi;  
+    }
 
 })
 
