8c9e2e752855a39abc9134cdddfb900e2c44a44b
LeahBasson <leahbasson54@gmail.com>
2024-05-17 09:20:05 +0200

    local

--- script/code.js
+++ script/code.js
@@ -1,15 +1,20 @@
-let result = document.querySelector(`[result]`)
+// the button
+let result = document.querySelector(`[result]`);
+
+// button function
 result.addEventListener('click', () => 
 {
-
+    // the values
     let weight = document.querySelector('[weight]').value 
     let height = document.querySelector('[height]').value
 
+    // Calculation
     let bmi = (weight / ((height*height)/10000)).toFixed(2);
 
+    // Display
     console.log(bmi);
 
-    document.querySelector(`[output]`).innerHTML= bmi;
+    document.querySelector(`[output]`).innerHTML= bmi; 
 
 })
 
